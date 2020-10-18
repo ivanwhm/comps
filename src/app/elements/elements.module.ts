6a6ea3bcd6dd704f651150b42fb0b49459de0ce7
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 19:46:08 -0300

    Creating a Placeholder Component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { ElementsRoutingModule } from './elements-routing.module';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent],
   imports: [CommonModule, ElementsRoutingModule],
 })
 export class ElementsModule {}
