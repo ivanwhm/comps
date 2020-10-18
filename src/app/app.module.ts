21a3394cc32f574e491d896e9c02e0c9d5fa748e
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 16:26:59 -0300

    Modules Exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { ViewsModule } from './views/views.module';
+import { ModsModule } from './mods/mods.module';
+import { CollectionsModule } from './collections/collections.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,7 +10,14 @@ import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CollectionsModule,
+    ElementsModule,
+    ModsModule,
+    ViewsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
