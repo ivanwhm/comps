e781e0cde1e017a53b6b6165d68a165b87cb4260
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 21:01:00 -0300

    Communicationg Down Table Data

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  @Input() data = [];
+  @Input() headers = [];
+
   constructor() {}
 
   ngOnInit(): void {}
