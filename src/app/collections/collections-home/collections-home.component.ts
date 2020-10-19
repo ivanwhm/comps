7546580eb6fbb7b9c679c2ef43c5090f0e4dd183
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 21:11:12 -0300

    Assembling the Table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -11,19 +11,26 @@ export class CollectionsHomeComponent implements OnInit {
       name: 'James',
       age: 24,
       job: 'Designer',
+      employed: true,
     },
     {
       name: 'Jill',
       age: 26,
       job: 'Engineer',
+      employed: false,
     },
     {
       name: 'Elyse',
       age: 25,
       job: 'Designer',
+      employed: true,
     },
   ];
   headers = [
+    {
+      key: 'employed',
+      label: 'Has a Job?',
+    },
     {
       key: 'name',
       label: 'Name',
