11c3d4cb388357576126c1996c9e09f5f0487a7e
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-01 13:38:51 -0300

    Opening the Modal

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick(): void {
+    this.modalOpen = !this.modalOpen;
+  }
 }
