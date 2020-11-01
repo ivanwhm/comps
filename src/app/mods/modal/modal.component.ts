d14ba459332956df6ea39f44130d054d5046499c
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-01 13:29:15 -0300

    Hiding the Modal with ngOnDestroy

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,14 +1,18 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   constructor(private readonly elementRef: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.elementRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    document.body.removeChild(this.elementRef.nativeElement);
   }
 }
