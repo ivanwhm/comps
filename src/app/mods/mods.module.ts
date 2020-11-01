414d8e951081c60605482853479452245747e9a3
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-01 12:49:32 -0300

    Modal Window Setup

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
+import { ModalComponent } from './modal/modal.component';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModsRoutingModule } from './mods-routing.module';
 
 @NgModule({
-  declarations: [ModsHomeComponent],
-  imports: [CommonModule, ModsRoutingModule],
+  declarations: [ModsHomeComponent, ModalComponent],
+  imports: [CommonModule, SharedModule, ModsRoutingModule],
 })
 export class ModsModule {}
