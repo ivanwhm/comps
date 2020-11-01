429249e00698bd6a74e1bd5d1a25e35a394a1f5f
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-01 14:26:15 -0300

    Expanding the Active Element

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -9,7 +9,17 @@ export class AccordionComponent implements OnInit {
   @Input()
   data = [];
 
+  openedItemIndex = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick(index: number): void {
+    if (this.openedItemIndex === index) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
+  }
 }
